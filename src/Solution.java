cbfbe8ed0161111000df734c07f7362e5d99d620
Juan Perez <jperez4432@gmail.com>
2021-06-26 02:02:55 -0500

    3rd

---
+++ src/Solution.java
@@ -0,0 +1,11 @@
+    public class Solution {
+        public static String repeatStr(final int repeat, final String string) {
+            return string.repeat(repeat);
+        }
+
+        public static void main(String[] args) {
+            System.out.println(repeatStr(3, "Hello"));
+            System.out.println(repeatStr(7, "Meowdy meow meowwwww"));
+        }
+    }
+
