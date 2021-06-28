66f4ba6b38470e0ae8a6f69793843dbabfd0c9c5
Juan Perez <jperez4432@gmail.com>
2021-06-27 21:43:22 -0500

    completed

--- src/BasicOperations.java
+++ src/BasicOperations.java
@@ -1,11 +1,15 @@
-public class BasicOperations
-{
-    public static Integer basicMath(String op, int v1, int v2)
-    {
 
-        System.out.printf("%d %s %d = ",v1, op, v2);
-        return 0;
-    }
+    public class BasicOperations{
+        public static Integer basicMath(String symbol, int x, int y){
+            switch (symbol){
+                case "+": return x+y;
+                case "-": return x-y;
+                case "*": return x*y;
+                case "/": return x/y;
+            }
+            return 0;
+        }
+
 
     public static void main(String[] args) {
         System.out.println(basicMath("+", 1, 2));
